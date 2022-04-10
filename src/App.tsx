45921554cab71c45a1f82a6a66027a804e9212bb
HenriqueSenaDev <luizteste656@gmail.com>
2022-04-09 21:58:03 -0300

    services-FtpGames/getGameList/getHighlightedGames

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./assets/styles/custom.scss";
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar";
+import FtpGames from "./services/FtpGames";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <Navbar />
-  );
+  useEffect(() => {
+    FtpGames.getHighlightedGames();
+  }, []);
+
+  return <Navbar />;
 }
 
 export default App;
